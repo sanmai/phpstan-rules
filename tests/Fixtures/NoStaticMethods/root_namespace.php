7fecaafa2ee4c642826859341fb7abc098fd7bab
Alexey Kopytko <alexey@kopytko.com>
2025-07-29 22:49:08 +0900

    xx

--- tests/Fixtures/NoStaticMethods/root_namespace.php
+++ tests/Fixtures/NoStaticMethods/root_namespace.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace {
-    class TestFixtures_NoStaticMethods_Foo
+    abstract class TestFixtures_NoStaticMethods_Foo
     {
         public static function bar() {} // One method is fine
         public static function baz() {} // ERROR: classes with private constructors can have as many static methods
